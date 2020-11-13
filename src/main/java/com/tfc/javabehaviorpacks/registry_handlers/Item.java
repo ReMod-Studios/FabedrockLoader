edc766c5816d443fd0fb03e2f49579848a7ae3cf
GiantLuigi4 <49770992+GiantLuigi4@users.noreply.github.com>
2020-11-13 13:53:03 +0000

    Update Item.java

--- src/main/java/com/tfc/javabehaviorpacks/registry_handlers/Item.java
+++ src/main/java/com/tfc/javabehaviorpacks/registry_handlers/Item.java
@@ -19,7 +19,7 @@ public class Item {
 			
 			while (sc.hasNextLine()) {
 				String line = sc.nextLine();
-				//Curse you too bridge, lol
+				//Curse you too bridge., lol
 				if (!line.startsWith("//")) {
 					s.append(line.trim());
 				}
