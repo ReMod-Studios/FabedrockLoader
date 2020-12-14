502584b0842ab82383197e15d5c4035eedb10618
GiantLuigi4 <49770992+giantluigi4@users.noreply.github.com>
2020-12-14 18:02:02 -0500

    update icon, add particle mapping getter

--- src/main/java/com/tfc/javabehaviorpacks/utils/assets_helpers/BedrockMapper.java
+++ src/main/java/com/tfc/javabehaviorpacks/utils/assets_helpers/BedrockMapper.java
@@ -12,6 +12,7 @@ public class BedrockMapper {
 	private static final HashMap<String, String> tabs = new HashMap<>();
 	private static final HashMap<String, String> sounds = new HashMap<>();
 	private static final HashMap<String, String> tags = new HashMap<>();
+	private static final HashMap<String, String> particles = new HashMap<>();
 	
 	static {
 		try {
@@ -19,31 +20,37 @@ public class BedrockMapper {
 			InputStream streamTabs = BedrockMapper.class.getClassLoader().getResourceAsStream("assets/java-behavior-packs/mappings/tab_map.properties");
 			InputStream streamSounds = BedrockMapper.class.getClassLoader().getResourceAsStream("assets/java-behavior-packs/mappings/sound_map.properties");
 			InputStream streamTags = BedrockMapper.class.getClassLoader().getResourceAsStream("assets/java-behavior-packs/mappings/tag_map.properties");
+			InputStream streamParticles = BedrockMapper.class.getClassLoader().getResourceAsStream("assets/java-behavior-packs/mappings/particle_map.properties");
 
 			assert streamItems != null;
 			assert streamTabs != null;
 			assert streamSounds != null;
 			assert streamTags != null;
+			assert streamParticles != null;
 
 			String mapItems = JavaBehaviorPacks.readStream(streamItems);
 			String mapTabs = JavaBehaviorPacks.readStream(streamTabs);
 			String mapSounds = JavaBehaviorPacks.readStream(streamSounds);
 			String mapTags = JavaBehaviorPacks.readStream(streamTags);
+			String mapParticles = JavaBehaviorPacks.readStream(streamParticles);
 
 			streamItems.close();
 			streamTabs.close();
 			streamSounds.close();
 			streamTags.close();
+			streamParticles.close();
 			
 			PropertiesReader readerItems = new PropertiesReader(mapItems);
 			PropertiesReader readerTabs = new PropertiesReader(mapTabs);
 			PropertiesReader readerSounds = new PropertiesReader(mapSounds);
 			PropertiesReader readerTags = new PropertiesReader(mapTags);
+			PropertiesReader readerParticles = new PropertiesReader(mapParticles);
 			
 			for (String s : readerItems.getEntries()) items.put(s, readerItems.getValue(s));
 			for (String s : readerTabs.getEntries()) tabs.put(s, readerTabs.getValue(s));
 			for (String s : readerSounds.getEntries()) sounds.put(s, readerSounds.getValue(s));
 			for (String s : readerTags.getEntries()) tags.put(s, readerTags.getValue(s));
+			for (String s : readerParticles.getEntries()) particles.put(s, readerParticles.getValue(s));
 		} catch (Throwable err) {
 			throw new RuntimeException(err);
 		}
@@ -59,6 +66,10 @@ public class BedrockMapper {
 	
 	public static String getMappedForSound(String bedrockName) {
 		return sounds.getOrDefault(bedrockName,bedrockName);
+	}
+	
+	public static String getMappedForParticle(String bedrockName) {
+		return particles.getOrDefault(bedrockName,bedrockName);
 	}
 	
 	public static ArrayList<String> getAllTags(String bedrockName) {
